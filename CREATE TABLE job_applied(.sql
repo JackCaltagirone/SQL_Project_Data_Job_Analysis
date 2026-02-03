dd7a8ebb9fffffe0f60d8e51476b875b76eb4bc2
Jack Caltagirone <35774493+JackCaltagirone@users.noreply.github.com>
2026-02-03 13:57:39 +0000

    Initial commit

---
+++ CREATE TABLE job_applied(.sql
@@ -0,0 +1,12 @@
+CREATE TABLE job_applied(
+    jon_id int,
+    application_sent_date date,
+    custin_resume boolean,
+    resume_file_name varchar(255),
+    cover_letter_sent boolean,
+    cover_letter_file_name varchar(255),
+    status varchar(50)
+);
+
+
+
