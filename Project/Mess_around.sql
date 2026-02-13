6b343ff36c217027f50591fb1bcff27ac00de17b
Jack Caltagirone <35774493+JackCaltagirone@users.noreply.github.com>
2026-02-13 12:34:41 +0000

    Slight changes added seperate workspace for more conplex data and problems

--- Project/Mess_around.sql
+++ Project/Mess_around.sql
@@ -37,3 +37,25 @@ ORDER BY
 
 LIMIT 10;
 
+
+SELECT
+    job_title_short,
+    Round(avg(salary_year_avg),2) as salary_avg,
+    Round(avg(salary_hour_avg), 2) as salary_avg_hour,
+    Round(avg(COALESCE(salary_year_avg, salary_hour_avg * 2080)), 2) AS effective_salary_avg
+
+from
+    job_postings_fact
+
+group by
+    job_title_short
+
+ORDER by
+    effective_salary_avg DESC NULLS LAST
+
+    
+LIMIT 10;
+
+
+
+
